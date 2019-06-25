1d7fda172c0af3aa9258533bfec2fe14684a63c3
zhangyongwang <20225614@qq.com>
2019-06-25 12:44:35 +0800

    改一处bug

--- src/main/java/com/github/hcsp/encapsulation/Main.java
+++ src/main/java/com/github/hcsp/encapsulation/Main.java
@@ -27,12 +27,14 @@ public class Main {
 
         student = deserialize(json);
     }
+
     // 序列化：将Student类转换成JSON字符串
     public static String serialize(Student student) {
         return JSON.toJSONString(student);
     }
+
     // 反序列化：将JSON字符串转换成Student对象
     public static Student deserialize(String json) {
-        return JSON.parseObject("{\"fail\":false,\"name\":\"张三\",\"retakingExam\":true,\"score\":60}", Student.class);
+        return JSON.parseObject(json, Student.class);
     }
 }
