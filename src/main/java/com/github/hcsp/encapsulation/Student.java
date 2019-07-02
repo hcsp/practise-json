4ce0aebf4f1ae5d1e650eabd3c36baf7ff93f7cc
XiongAmao <onycat@live.cn>
2019-07-02 23:34:12 +0800

    Practise json.

--- src/main/java/com/github/hcsp/encapsulation/Student.java
+++ src/main/java/com/github/hcsp/encapsulation/Student.java
@@ -10,4 +10,40 @@ public class Student {
 
     /** 分数 */
     private int score;
+
+    private boolean fail;
+
+    public boolean isFail(){
+        return this.fail;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public boolean isRetakingExam() {
+        return retakingExam;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setRetakingExam(boolean retakingExam) {
+        this.retakingExam = retakingExam;
+    }
+
+    public void setScore(int score) {
+        if (score < 60 && score >= 0) {
+            this.fail = true;
+        } else {
+            this.fail = false;
+        }
+        this.score = score;
+    }
+
 }
