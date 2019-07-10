9aba4ea3a59a458745c2572edbef8b68e12feeb5
liboyang <huagongyilby@126.com>
2019-07-10 16:50:17 +0800

    忘记把test的注释删掉了

--- src/test/java/com/github/hcsp/encapsulation/MainTest.java
+++ src/test/java/com/github/hcsp/encapsulation/MainTest.java
@@ -1,35 +1,35 @@
-//package com.github.hcsp.encapsulation;
-//
-//import java.lang.reflect.Field;
-//import java.lang.reflect.Modifier;
-//import java.util.stream.Stream;
-//import org.junit.jupiter.api.Assertions;
-//import org.junit.jupiter.api.Test;
-//
-//public class MainTest {
-//    @Test
-//    public void allFieldsArePrivate() {
-//        Assertions.assertTrue(
-//                Stream.of(com.github.hcsp.encapsulation.Student.class.getDeclaredFields())
-//                        .map(Field::getModifiers)
-//                        .allMatch(Modifier::isPrivate));
-//    }
-//
-//    @Test
-//    public void canSerializeAndDeserialize() {
-//        Student s = new Student();
-//        s.setName("张三");
-//        s.setRetakingExam(true);
-//        s.setScore(59);
-//        String json = com.alibaba.fastjson.JSON.toJSONString(s);
-//
-//        Assertions.assertTrue(json.contains("\"fail\""));
-//
-//        s = com.alibaba.fastjson.JSON.parseObject(json, Student.class);
-//
-//        Assertions.assertEquals("张三", s.getName());
-//        Assertions.assertEquals(true, s.isRetakingExam());
-//        Assertions.assertEquals(59, s.getScore());
-//        Assertions.assertEquals(true, s.isFail());
-//    }
-//}
+package com.github.hcsp.encapsulation;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
+import java.util.stream.Stream;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class MainTest {
+    @Test
+    public void allFieldsArePrivate() {
+        Assertions.assertTrue(
+                Stream.of(com.github.hcsp.encapsulation.Student.class.getDeclaredFields())
+                        .map(Field::getModifiers)
+                        .allMatch(Modifier::isPrivate));
+    }
+
+    @Test
+    public void canSerializeAndDeserialize() {
+        Student s = new Student();
+        s.setName("张三");
+        s.setRetakingExam(true);
+        s.setScore(59);
+        String json = com.alibaba.fastjson.JSON.toJSONString(s);
+
+        Assertions.assertTrue(json.contains("\"fail\""));
+
+        s = com.alibaba.fastjson.JSON.parseObject(json, Student.class);
+
+        Assertions.assertEquals("张三", s.getName());
+        Assertions.assertEquals(true, s.isRetakingExam());
+        Assertions.assertEquals(59, s.getScore());
+        Assertions.assertEquals(true, s.isFail());
+    }
+}
