9a9864b26374e89e9635084188ee06d66067d2fe
sutaojie <sutaojie@gmail.com>
2019-07-11 11:59:12 +0800

    json序列化 (#24)
    json序列化 (#24)

--- src/main/java/com/github/hcsp/encapsulation/Student.java
+++ src/main/java/com/github/hcsp/encapsulation/Student.java
@@ -10,4 +10,32 @@ public class Student {
 
     /** 分数 */
     private int score;
+
+    public boolean isFail() {
+        return this.score < 60;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public boolean isRetakingExam() {
+        return retakingExam;
+    }
+
+    public void setRetakingExam(boolean retakingExam) {
+        this.retakingExam = retakingExam;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
 }
