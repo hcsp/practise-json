da910d9d537f03f27ee9b746979374120b866600
lis <liyu.italy@hotmail.com>
2019-07-16 19:43:43 +0800

    json practice

--- src/main/java/com/github/hcsp/encapsulation/Main.java
+++ src/main/java/com/github/hcsp/encapsulation/Main.java
@@ -1,5 +1,10 @@
 package com.github.hcsp.encapsulation;
 
+import java.io.IOException;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 public class Main {
     /*
          假设你正在为学校开发一个学生分数记录系统
@@ -14,19 +19,29 @@ public class Main {
          1. 设计并完成Student类
          2. 挑选一种你喜欢的JSON类库，完成序列化/反序列化的方法
     */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         Student student = new Student();
         student.setName("张三");
         student.setScore(60);
         student.setRetakingExam(true);
+
         String json = serialize(student);
 
         System.out.println(json);
 
         student = deserialize(json);
+        System.out.println(student);
     }
     // 序列化：将Student类转换成JSON字符串
-    public static String serialize(Student student) {}
-    // 反序列化：将JSON字符串转换成Student对象
-    public static Student deserialize(String json) {}
+    public static String serialize(Student student) throws JsonProcessingException {
+
+        // Java object to JSON string
+        return new ObjectMapper().writeValueAsString(student);
+    }
+   // 反序列化：将JSON字符串转换成Student对象
+    public static Student deserialize(String json) throws IOException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+        return objectMapper.readValue(json, Student.class);
+    }
 }
--- src/main/java/com/github/hcsp/encapsulation/Student.java
+++ src/main/java/com/github/hcsp/encapsulation/Student.java
@@ -1,5 +1,7 @@
 package com.github.hcsp.encapsulation;
 
+import java.io.Serializable;
+
 public class Student {
     // 请按照Main类的要求，补全本类
     /** 姓名 */
@@ -10,4 +12,42 @@ public class Student {
 
     /** 分数 */
     private int score;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public boolean isRetakingExam() {
+        return retakingExam;
+    }
+
+    public void setRetakingExam(boolean retakingExam) {
+        this.retakingExam = retakingExam;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+
+    public boolean isFail() {
+        int passingGrade = 60;
+        return this.score < passingGrade;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", retakingExam=" + retakingExam +
+                ", score=" + score +
+                '}';
+    }
 }
