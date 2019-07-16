b7db17f3a905190c809eb1ac7d10639587fedbd9
shiliyu <34638563+shiliyu@users.noreply.github.com>
2019-07-16 20:10:04 +0800

    Json practice (#26)
    Json practice (#26)

--- src/main/java/com/github/hcsp/encapsulation/Student.java
+++ src/main/java/com/github/hcsp/encapsulation/Student.java
@@ -10,4 +10,42 @@ public class Student {
 
     /** 分数 */
     private int score;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public boolean isRetakingExam() {
+        return retakingExam;
+    }
+
+    public void setRetakingExam(boolean retakingExam) {
+        this.retakingExam = retakingExam;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+
+    public boolean isFail() {
+        int passingGrade = 60;
+        return this.score < passingGrade;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", retakingExam=" + retakingExam +
+                ", score=" + score +
+                '}';
+    }
 }
