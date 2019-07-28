afa2003efddb8189c8d8f10de8db74587f961b6d
Wang Peng <947034046@qq.com>
2019-07-29 00:40:11 +0800

    Practise json.

--- src/main/java/com/github/hcsp/encapsulation/Student.java
+++ src/main/java/com/github/hcsp/encapsulation/Student.java
@@ -10,4 +10,34 @@ public class Student {
 
     /** 分数 */
     private int score;
+
+    /** 是否挂科 **/
+    private boolean fail;
+
+    // 定义 Getter 和 Setter
+    public String getName() {
+        return name;
+    }
+    public Student setName(String name) {
+        this.name = name;
+        return this;
+    }
+    public boolean isRetakingExam() {
+        return retakingExam;
+    }
+    public Student setRetakingExam(boolean retakingExam) {
+        this.retakingExam = retakingExam;
+        return this;
+    }
+    public int getScore() {
+        return score;
+    }
+    public Student setScore(int score) {
+        this.score = score;
+        this.fail = score < 60;
+        return this;
+    }
+    public boolean isFail() {
+        return fail;
+    }
 }
