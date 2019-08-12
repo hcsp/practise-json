f1d34b5449458a0f9a6eb4654c6e4fd92efae414
Mingru Li <limingru1991@gmail.com>
2019-08-12 09:45:59 +0800

    序列化和反序列化 (#40)
    序列化和反序列化 (#40)

--- src/main/java/com/github/hcsp/encapsulation/Student.java
+++ src/main/java/com/github/hcsp/encapsulation/Student.java
@@ -10,4 +10,44 @@ public class Student {
 
     /** 分数 */
     private int score;
+
+    private boolean fail;
+
+    public String getName() {
+        return name;
+    }
+
+    private void setFail(boolean fail) {
+        this.fail = fail;
+    }
+
+    public boolean isFail() {
+
+        return fail;
+    }
+
+    public boolean isRetakingExam() {
+        return retakingExam;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setRetakingExam(boolean retakingExam) {
+        this.retakingExam = retakingExam;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+        if (score < 60) {
+            setFail(true);
+        } else {
+        	setFail(false);
+        }
+    }
 }
