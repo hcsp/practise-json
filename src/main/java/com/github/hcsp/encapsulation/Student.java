352da272852e9c4643da84ed95656d6e7b9de832
PingAn_NetBar <840495823@qq.com>
2019-08-23 00:39:17 +0800

    利用第三方类库来实现序列化和反序列化 (#48)
    利用第三方类库来实现序列化和反序列化 (#48)

--- src/main/java/com/github/hcsp/encapsulation/Student.java
+++ src/main/java/com/github/hcsp/encapsulation/Student.java
@@ -2,6 +2,40 @@ package com.github.hcsp.encapsulation;
 
 public class Student {
     // 请按照Main类的要求，补全本类
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public boolean isRetakingExam() {
+        return retakingExam;
+    }
+
+    public void setRetakingExam(boolean retakingExam) {
+        this.retakingExam = retakingExam;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+
+    public boolean isFail() {
+        if(score < 60){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+
     /** 姓名 */
     private String name;
 
@@ -10,4 +44,7 @@ public class Student {
 
     /** 分数 */
     private int score;
+
+
+    private boolean fail;
 }
