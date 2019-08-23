40103799b2f0c7504844f5ae49f8b825ebeec14c
YakumoAoi <416910533@qq.com>
2019-08-23 12:45:33 +0900

    json_serialize_deserialize (#50)
    json_serialize_deserialize (#50)

--- src/main/java/com/github/hcsp/encapsulation/Main.java
+++ src/main/java/com/github/hcsp/encapsulation/Main.java
@@ -1,5 +1,7 @@
 package com.github.hcsp.encapsulation;
 
+import com.google.gson.Gson;
+
 public class Main {
     /*
          假设你正在为学校开发一个学生分数记录系统
@@ -25,8 +27,17 @@ public class Main {
 
         student = deserialize(json);
     }
+
     // 序列化：将Student类转换成JSON字符串
-    public static String serialize(Student student) {}
+    public static String serialize(Student student) {
+        Gson gson = new Gson();
+        return gson.toJson(student);
+    }
+
     // 反序列化：将JSON字符串转换成Student对象
-    public static Student deserialize(String json) {}
+    public static Student deserialize(String json) {
+        Gson gson = new Gson();
+        Student student1 = gson.fromJson(json, Student.class);
+        return student1;
+    }
 }
