cba150df97d0f9d3090ef68691ec1ab9cd1523e7
JiaXinWang61 <55199085+JiaXinWang61@users.noreply.github.com>
2019-10-29 11:43:24 +0800

    Practise json (#65)
    Practise json (#65)

--- src/main/java/com/github/hcsp/encapsulation/Main.java
+++ src/main/java/com/github/hcsp/encapsulation/Main.java
@@ -1,5 +1,7 @@
 package com.github.hcsp.encapsulation;
 
+import com.alibaba.fastjson.JSON;
+
 public class Main {
     /*
          假设你正在为学校开发一个学生分数记录系统
@@ -24,9 +26,16 @@ public class Main {
         System.out.println(json);
 
         student = deserialize(json);
+        System.out.println(student);
+
     }
     // 序列化：将Student类转换成JSON字符串
-    public static String serialize(Student student) {}
+    public static String serialize(Student student) {
+        return JSON.toJSONString(student);
+    }
+
     // 反序列化：将JSON字符串转换成Student对象
-    public static Student deserialize(String json) {}
+    public static Student deserialize(String json) {
+        return JSON.parseObject(json, Student.class);
+    }
 }
--- src/main/java/com/github/hcsp/encapsulation/Student.java
+++ src/main/java/com/github/hcsp/encapsulation/Student.java
@@ -2,12 +2,64 @@ package com.github.hcsp.encapsulation;
 
 public class Student {
     // 请按照Main类的要求，补全本类
-    /** 姓名 */
+    /**
+     * 姓名
+     */
     private String name;
 
-    /** 是否重考。true为重考，falase为非重考。 */
+    /**
+     * 是否重考。true为重考，falase为非重考。
+     */
     private boolean retakingExam;
 
-    /** 分数 */
+    /**
+     * 分数
+     */
     private int score;
+
+    public Student() {
+    }
+
+    public Student(String name, boolean retakingExam, int score) {
+        this.name = name;
+        this.retakingExam = retakingExam;
+        this.score = score;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public boolean isRetakingExam() {
+        return retakingExam;
+    }
+
+    public void setRetakingExam(boolean retakingExam) {
+        this.retakingExam = retakingExam;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+
+    public boolean isFail() {
+        return this.score<60;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", retakingExam=" + retakingExam +
+                ", score=" + score +
+                '}';
+    }
 }
