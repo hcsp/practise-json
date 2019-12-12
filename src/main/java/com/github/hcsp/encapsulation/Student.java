22c6b723e60ca8cbb597509f787bba74727f1b29
xuepan-summer <54898607+xuepan-summer@users.noreply.github.com>
2019-12-12 09:12:58 +0800

    json序列化及反序列化 (#99)
    json序列化及反序列化 (#99)

--- src/main/java/com/github/hcsp/encapsulation/Student.java
+++ src/main/java/com/github/hcsp/encapsulation/Student.java
@@ -10,4 +10,29 @@ public class Student {
 
     /** 分数 */
     private int score;
+
+    public void setName(String name){
+        this.name = name;
+    }
+    public void setScore(int score){
+        this.score = score;
+    }
+    public void setRetakingExam(boolean retakingExam){
+        this.retakingExam = retakingExam;
+    }
+    public String getName(){
+        return name;
+    }
+    public int getScore(){
+        return score;
+    }
+    public boolean isRetakingExam(){
+        return retakingExam;
+    }
+    public boolean isFail(){
+        if (score < 60) {
+            return true;
+        }
+        return false;
+    }
 }
