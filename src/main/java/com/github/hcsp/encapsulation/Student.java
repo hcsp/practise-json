464e67fa6446c1a7863bf78621966a6ad369d0e4
jr10101010 <57735218+jr10101010@users.noreply.github.com>
2019-12-30 18:58:29 +0800

    JSON序列化/反序列化实战 (#112)
    JSON序列化/反序列化实战 (#112)

--- src/main/java/com/github/hcsp/encapsulation/Student.java
+++ src/main/java/com/github/hcsp/encapsulation/Student.java
@@ -10,4 +10,36 @@ public class Student {
 
     /** 分数 */
     private int score;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setRetakingExam(boolean retakingExam) {
+        this.retakingExam = retakingExam;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public boolean isRetakingExam() {
+        return retakingExam;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+
+    public boolean isFail() {
+        if (this.score < 60) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
