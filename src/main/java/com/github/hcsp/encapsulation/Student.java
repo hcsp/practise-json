bde5c94bd0d3a6fca80ba936998e754864c2c2de
xdc <1434412784@qq.com>
2020-01-01 17:49:52 +0800

    完成JSON序列化/反序列化实战

--- src/main/java/com/github/hcsp/encapsulation/Student.java
+++ src/main/java/com/github/hcsp/encapsulation/Student.java
@@ -2,12 +2,51 @@ package com.github.hcsp.encapsulation;
 
 public class Student {
     // 请按照Main类的要求，补全本类
-    /** 姓名 */
+    /**
+     * 姓名
+     */
     private String name;
 
-    /** 是否重考。true为重考，falase为非重考。 */
+    /**
+     * 是否重考。true为重考，falase为非重考。
+     */
     private boolean retakingExam;
 
-    /** 分数 */
+    /**
+     * 分数
+     */
     private int score;
+
+    private boolean fail;
+
+    public boolean isFail() {
+        return fail;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public boolean isRetakingExam() {
+        return retakingExam;
+    }
+
+    public void setRetakingExam(boolean retakingExam) {
+        this.retakingExam = retakingExam;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        //根据成绩自动设置是否挂科
+        fail = score < 60 && score >= 0;
+
+        this.score = score;
+    }
 }
