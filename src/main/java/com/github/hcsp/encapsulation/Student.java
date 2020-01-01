5b51248e89158fd872c55d8fc5f2640792299598
xdc <1434412784@qq.com>
2020-01-01 18:35:20 +0800

    再次补充Student类的fail字段的说明

--- src/main/java/com/github/hcsp/encapsulation/Student.java
+++ src/main/java/com/github/hcsp/encapsulation/Student.java
@@ -19,6 +19,10 @@ public class Student {
 
     /**
      * 是否挂科。true为挂科，false为没有挂科
+     *
+     * 补充：我个人认为，既然和前端约定了JSON接口格式，
+     * 那Student类，应该也要有fail字段吧，
+     * 即使实际操作只看getter/setter方法
      */
     private boolean fail;
 
