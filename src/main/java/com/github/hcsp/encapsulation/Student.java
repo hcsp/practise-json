00df5f76325812dfc9bb1b0a5d843fe61e8e8fbb
richard Hu <huyongjinnuaa@163.com>
2020-01-05 17:33:48 +0800

    序列化与反序列化实战 (#120)
    序列化与反序列化实战 (#120)

--- src/main/java/com/github/hcsp/encapsulation/Student.java
+++ src/main/java/com/github/hcsp/encapsulation/Student.java
@@ -10,4 +10,33 @@ public class Student {
 
     /** 分数 */
     private int score;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public boolean isRetakingExam() {
+        return retakingExam;
+    }
+
+    public void setRetakingExam(boolean retakingExam) {
+        this.retakingExam = retakingExam;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+
+    public boolean isFail() {
+        return score < 60;
+
+    }
 }
