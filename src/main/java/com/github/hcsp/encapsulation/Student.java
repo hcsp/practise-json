beb07e8e575345626db3aae7a300de2efabbdcf9
蒋锐 <448421908@qq.com>
2020-02-05 11:21:05 +0800

    序列化与反序列化2

--- src/main/java/com/github/hcsp/encapsulation/Student.java
+++ src/main/java/com/github/hcsp/encapsulation/Student.java
@@ -8,6 +8,41 @@ public class Student {
     /** 是否重考。true为重考，falase为非重考。 */
     private boolean retakingExam;
 
+    private boolean fail;
+
     /** 分数 */
     private int score;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public boolean isRetakingExam() {
+        return retakingExam;
+    }
+
+    public void setRetakingExam(boolean retakingExam) {
+        this.retakingExam = retakingExam;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+
+    public boolean isFail() {
+
+        return fail;
+    }
+
+    public void setFail(boolean fail) {
+        this.fail = fail;
+    }
 }
