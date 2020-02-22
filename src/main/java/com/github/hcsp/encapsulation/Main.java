aacb876df66653d652766a455329eb887e4d345e
walkerxin <1464193658@qq.com>
2020-02-22 23:39:26 +0800

    revert score value

--- src/main/java/com/github/hcsp/encapsulation/Main.java
+++ src/main/java/com/github/hcsp/encapsulation/Main.java
@@ -20,7 +20,7 @@ public class Main {
     public static void main(String[] args) {
         Student student = new Student();
         student.setName("张三");
-        student.setScore(59);
+        student.setScore(60);
         student.setRetakingExam(true);
         String json = serialize(student);
 
