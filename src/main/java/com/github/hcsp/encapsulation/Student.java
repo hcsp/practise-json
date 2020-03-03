6dcd39c52b35deb2cfa9a2af5a8ed0579c804452
Airtoit <51944383+Airtoit@users.noreply.github.com>
2020-03-03 17:19:44 +0800

    对象和json之间的序列化和反序列化 (#147)
    对象和json之间的序列化和反序列化 (#147)

--- src/main/java/com/github/hcsp/encapsulation/Student.java
+++ src/main/java/com/github/hcsp/encapsulation/Student.java
@@ -1,13 +1,49 @@
 package com.github.hcsp.encapsulation;
 
 public class Student {
-    // 请按照Main类的要求，补全本类
-    /** 姓名 */
-    private String name;
+	// 请按照Main类的要求，补全本类
+	/** 姓名 */
+	private String name;
 
-    /** 是否重考。true为重考，falase为非重考。 */
-    private boolean retakingExam;
+	/** 是否重考。true为重考，false为非重考。 */
+	private boolean retakingExam;
 
-    /** 分数 */
-    private int score;
+	/** 分数 */
+	private int score;
+
+	/** 是否挂科。true为挂科，false为通过。 */
+	private boolean fail;
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public void setRetakingExam(boolean retakingExam) {
+		this.retakingExam = retakingExam;
+	}
+
+	public void setScore(int score) {
+		this.score = score;
+		if (score < 60) {
+			fail = true;
+		} else {
+			fail = false;
+		}
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public boolean isRetakingExam() {
+		return retakingExam;
+	}
+
+	public int getScore() {
+		return score;
+	}
+
+	public boolean isFail() {
+		return fail;
+	}
 }
