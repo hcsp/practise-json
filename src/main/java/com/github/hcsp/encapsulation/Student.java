9114eae78193be810b0421fbeee52d3124687359
yanghanjie <yang_hanjie@163.com>
2020-03-06 01:45:06 -0500

    Serialize/Deserialize using FastJSON

--- src/main/java/com/github/hcsp/encapsulation/Student.java
+++ src/main/java/com/github/hcsp/encapsulation/Student.java
@@ -2,12 +2,61 @@ package com.github.hcsp.encapsulation;
 
 public class Student {
     // 请按照Main类的要求，补全本类
-    /** 姓名 */
+    /**
+     * 姓名
+     */
     private String name;
 
-    /** 是否重考。true为重考，falase为非重考。 */
+    /**
+     * 是否重考。true为重考，false为非重考。
+     */
     private boolean retakingExam;
 
-    /** 分数 */
+    /**
+     * 分数
+     */
     private int score;
+
+    /**
+     * 是否挂科，true为挂科，false为没挂科。
+     */
+    private boolean fail;
+
+    public boolean isFail() {
+        return fail;
+    }
+
+    private void setFail(boolean fail) {
+        this.fail = fail;
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public boolean isRetakingExam() {
+        return retakingExam;
+    }
+
+    public void setRetakingExam(boolean retakingExam) {
+        this.retakingExam = retakingExam;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+        if (score >= 60) {
+            this.setFail(false);
+        } else {
+            this.setFail(true);
+        }
+    }
 }
