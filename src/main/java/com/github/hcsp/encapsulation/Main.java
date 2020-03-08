372f57ac10c9d882ee7d5d6d8f01bb9f657bea17
bgzsqqwd123 <a15146980388@163.com>
2020-03-08 18:25:00 +0800

    JSON序列化/反序列化

--- src/main/java/com/github/hcsp/encapsulation/Main.java
+++ src/main/java/com/github/hcsp/encapsulation/Main.java
@@ -1,19 +1,21 @@
 package com.github.hcsp.encapsulation;
 
+import com.alibaba.fastjson.JSON;
+
 public class Main {
     /*
-         假设你正在为学校开发一个学生分数记录系统
-         你和前端约定的JSON接口格式是：
-         {
-           "name": "张三",
-           "retakingExam": true,
-           "score": 59,
-           "fail": true // 是否挂科，如果分数低于60则返回true，代表挂科
-         }
-         请：
-         1. 设计并完成Student类
-         2. 挑选一种你喜欢的JSON类库，完成序列化/反序列化的方法
-    */
+             假设你正在为学校开发一个学生分数记录系统
+             你和前端约定的JSON接口格式是：
+             {
+               "name": "张三",
+               "retakingExam": true,
+               "score": 59,
+               "fail": true // 是否挂科，如果分数低于60则返回true，代表挂科
+             }
+             请：
+             1. 设计并完成Student类
+             2. 挑选一种你喜欢的JSON类库，完成序列化/反序列化的方法
+        */
     public static void main(String[] args) {
         Student student = new Student();
         student.setName("张三");
@@ -25,8 +27,14 @@ public class Main {
 
         student = deserialize(json);
     }
+
     // 序列化：将Student类转换成JSON字符串
-    public static String serialize(Student student) {}
+    public static String serialize(Student student) {
+        return JSON.toJSONString(student);
+    }
+
     // 反序列化：将JSON字符串转换成Student对象
-    public static Student deserialize(String json) {}
+    public static Student deserialize(String json) {
+        return JSON.parseObject(json, Student.class);
+    }
 }
