ab15e119143eb7a52879a2da9f6fad231acae28a
daimon0809 <Dp!@0809#$>
2020-03-19 00:28:54 +0800

    JSON序列化/反序列化实战

--- src/main/java/com/github/hcsp/encapsulation/Student.java
+++ src/main/java/com/github/hcsp/encapsulation/Student.java
@@ -10,4 +10,41 @@ public class Student {
 
     /** 分数 */
     private int score;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public boolean isRetakingExam() {
+        return retakingExam;
+    }
+
+    public void setRetakingExam(boolean retakingExam) {
+        this.retakingExam = retakingExam;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+
+    public boolean isFail() {
+        return score < 60;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", retakingExam=" + retakingExam +
+                ", score=" + score +
+                '}';
+    }
 }
