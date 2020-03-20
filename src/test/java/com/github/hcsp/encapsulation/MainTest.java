294145a24d986107cce4eada7a0c1e265a2906c8
XinleZhou <2320789321>
2020-03-20 17:55:08 +0800

    adasfgvsdg

--- src/test/java/com/github/hcsp/encapsulation/MainTest.java
+++ src/test/java/com/github/hcsp/encapsulation/MainTest.java
@@ -3,6 +3,7 @@ package com.github.hcsp.encapsulation;
 import java.lang.reflect.Field;
 import java.lang.reflect.Modifier;
 import java.util.stream.Stream;
+
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
