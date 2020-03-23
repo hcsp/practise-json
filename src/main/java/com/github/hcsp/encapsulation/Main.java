70997422ba94e1f135b4f003368ba9f3a7719fbe
wuzhenquan <782754354@qq.com>
2020-03-23 22:12:13 +0800

    JSON序列化/反序列化实战

--- src/main/java/com/github/hcsp/encapsulation/Main.java
+++ src/main/java/com/github/hcsp/encapsulation/Main.java
@@ -19,9 +19,9 @@ public class Main {
     public static void main(String[] args) {
         Student student = new Student();
         student.setName("张三");
-        student.setScore(60);
+        student.setScore(59);
         student.setRetakingExam(true);
-        student.setFail(false);
+        student.setFail(true);
         String json = serialize(student);
 
         System.out.println(json);
