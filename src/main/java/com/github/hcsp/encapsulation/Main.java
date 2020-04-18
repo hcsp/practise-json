7b5c0e4b42ba3a00a24baf09c18673c4c06209b7
HP <591639554@qq.com>
2020-04-18 15:47:55 +0800

    序列化与反序列化

--- src/main/java/com/github/hcsp/encapsulation/Main.java
+++ src/main/java/com/github/hcsp/encapsulation/Main.java
@@ -1,5 +1,8 @@
 package com.github.hcsp.encapsulation;
 
+import com.alibaba.fastjson.JSON;
+
+
 public class Main {
     /*
          假设你正在为学校开发一个学生分数记录系统
@@ -26,7 +29,11 @@ public class Main {
         student = deserialize(json);
     }
     // 序列化：将Student类转换成JSON字符串
-    public static String serialize(Student student) {}
+    public static String serialize(Student student) {
+        return JSON.toJSONString(student);
+    }
     // 反序列化：将JSON字符串转换成Student对象
-    public static Student deserialize(String json) {}
+    public static Student deserialize(String json) {
+        return JSON.parseObject(json,Student.class);
+    }
 }
