af6e0c43aa0eecd8fde6b5bfdc1320ee6324c93d
hanchenhao <hanchenhao@vip.qq.com>
2020-05-23 16:28:11 +0800

    fastjson and javabean (#185)
    fastjson and javabean (#185)

--- src/main/java/com/github/hcsp/encapsulation/Student.java
+++ src/main/java/com/github/hcsp/encapsulation/Student.java
@@ -8,6 +8,43 @@ public class Student {
     /** 是否重考。true为重考，falase为非重考。 */
     private boolean retakingExam;
 
+    /** 是否挂科 */
+    private boolean fail;
+
+
     /** 分数 */
     private int score;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public boolean isRetakingExam() {
+        return retakingExam;
+    }
+
+    public void setRetakingExam(boolean retakingExam) {
+        this.retakingExam = retakingExam;
+    }
+
+    public boolean isFail() {
+        return fail;
+    }
+
+    public void setFail(boolean fail) {
+        this.fail = fail;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+        this.fail = score < 60;
+    }
 }
