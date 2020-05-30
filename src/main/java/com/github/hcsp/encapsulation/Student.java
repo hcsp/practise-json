f6acafb9afbaa20e63a51e4d3f97d956b625bee3
BenHao Huang <33121651+Lazzben@users.noreply.github.com>
2020-05-30 15:40:57 +0800

    JSON序列化/反序列化实战 (#187)
    JSON序列化/反序列化实战 (#187)

--- src/main/java/com/github/hcsp/encapsulation/Student.java
+++ src/main/java/com/github/hcsp/encapsulation/Student.java
@@ -5,9 +5,40 @@ public class Student {
     /** 姓名 */
     private String name;
 
-    /** 是否重考。true为重考，falase为非重考。 */
+    /** 是否重考。true为重考，false为非重考。 */
     private boolean retakingExam;
 
     /** 分数 */
     private int score;
+
+    public boolean isFail(){
+        if(score < 60){
+            return true;
+        }
+        return false;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public boolean isRetakingExam() {
+        return retakingExam;
+    }
+
+    public void setRetakingExam(boolean retakingExam) {
+        this.retakingExam = retakingExam;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
 }
