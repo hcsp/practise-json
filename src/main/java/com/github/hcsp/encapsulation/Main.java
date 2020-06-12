8a0601a63e36e6c1d719d0f27d550ffd690064c2
ZEJIA LIU <53506531+ZEJIA-LIU@users.noreply.github.com>
2020-06-12 22:13:57 +0800

    JSON序列化/反序列化实战 (#190)
    JSON序列化/反序列化实战 (#190)

--- src/main/java/com/github/hcsp/encapsulation/Main.java
+++ src/main/java/com/github/hcsp/encapsulation/Main.java
@@ -1,5 +1,7 @@
 package com.github.hcsp.encapsulation;
 
+import com.alibaba.fastjson.JSON;
+
 public class Main {
     /*
          假设你正在为学校开发一个学生分数记录系统
@@ -20,13 +22,17 @@ public class Main {
         student.setScore(60);
         student.setRetakingExam(true);
         String json = serialize(student);
-
         System.out.println(json);
-
         student = deserialize(json);
     }
+
     // 序列化：将Student类转换成JSON字符串
-    public static String serialize(Student student) {}
+    public static String serialize(Student student) {
+        return JSON.toJSONString(student);
+    }
+
     // 反序列化：将JSON字符串转换成Student对象
-    public static Student deserialize(String json) {}
+    public static Student deserialize(String json) {
+        return JSON.parseObject("{\"fail\":false,\"name\":\"张三\",\"retakingExam\":true,\"score\":60}", Student.class);
+    }
 }
