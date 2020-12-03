9d3a09d8900bcb4cf5f2f7f25e5579f03dfbb870
weiranyi <67884344+weiranyi@users.noreply.github.com>
2020-12-03 10:35:11 +0800

    补充了Main类的序列化和反序列化，补全了Student中代码的setter/getter方法 (#215)
    补充了Main类的序列化和反序列化，补全了Student中代码的setter/getter方法 (#215)

--- src/main/java/com/github/hcsp/encapsulation/Main.java
+++ src/main/java/com/github/hcsp/encapsulation/Main.java
@@ -1,5 +1,8 @@
 package com.github.hcsp.encapsulation;
 
+
+import com.alibaba.fastjson.JSON;
+
 public class Main {
     /*
          假设你正在为学校开发一个学生分数记录系统
@@ -22,11 +25,18 @@ public class Main {
         String json = serialize(student);
 
         System.out.println(json);
-
         student = deserialize(json);
+
     }
+
     // 序列化：将Student类转换成JSON字符串
-    public static String serialize(Student student) {}
+    public static String serialize(Student student) {
+        return JSON.toJSONString(student);
+    }
+
     // 反序列化：将JSON字符串转换成Student对象
-    public static Student deserialize(String json) {}
+    public static Student deserialize(String json) {
+        return JSON.parseObject(json, Student.class);
+    }
+
 }
