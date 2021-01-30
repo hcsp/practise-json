c04d120e05c8160f129f0f38bad07286cdcfa219
Apolo Du <apolodu@ApolodeMacBook-Air.local>
2021-01-30 20:32:57 +0800

    update

--- src/test/java/com/github/hcsp/encapsulation/MainTest.java
+++ src/test/java/com/github/hcsp/encapsulation/MainTest.java
@@ -3,6 +3,7 @@ package com.github.hcsp.encapsulation;
 import java.lang.reflect.Field;
 import java.lang.reflect.Modifier;
 import java.util.stream.Stream;
+import com.alibaba.fastjson.JSON;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -21,11 +22,11 @@ public class MainTest {
         s.setName("张三");
         s.setRetakingExam(true);
         s.setScore(59);
-        String json = com.alibaba.fastjson.JSON.toJSONString(s);
+        String json = JSON.toJSONString(s);
 
         Assertions.assertTrue(json.contains("\"fail\""));
 
-        s = com.alibaba.fastjson.JSON.parseObject(json, Student.class);
+        s = JSON.parseObject(json, Student.class);
 
         Assertions.assertEquals("张三", s.getName());
         Assertions.assertEquals(true, s.isRetakingExam());
