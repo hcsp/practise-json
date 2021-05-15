14a6c852627bde24ae9159acda75ba42cdd33b1b
sltap <39816348+sltap@users.noreply.github.com>
2021-05-15 14:07:49 +0800

    11.1.4 (#226)
    11.1.4 (#226)

--- src/main/java/com/github/hcsp/encapsulation/Student.java
+++ src/main/java/com/github/hcsp/encapsulation/Student.java
@@ -4,10 +4,29 @@ public class Student {
     // 请按照Main类的要求，补全本类
     /** 姓名 */
     private String name;
-
+    public void setName(String name){
+        this.name=name;
+    }
     /** 是否重考。true为重考，falase为非重考。 */
     private boolean retakingExam;
-
+    public void setRetakingExam(boolean b){
+        this.retakingExam=b;
+    }
     /** 分数 */
     private int score;
+    public void setScore(int score){
+        this.score=score;
+    }
+    public String getName(){
+        return this.name;
+    }
+    public boolean isRetakingExam(){
+        return this.retakingExam;
+    }
+    public int getScore(){
+        return this.score;
+    }
+    public boolean isFail(){
+        return getScore()<60;
+    }
 }
