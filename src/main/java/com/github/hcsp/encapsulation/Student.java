28ab36dffca891caa18395240b4f507a982a7a0e
AdFeng <3200185362@qq.com>
2021-10-05 20:56:38 +0800

    完成作业

--- src/main/java/com/github/hcsp/encapsulation/Student.java
+++ src/main/java/com/github/hcsp/encapsulation/Student.java
@@ -10,4 +10,44 @@ public class Student {
 
     /** 分数 */
     private int score;
-}
+
+    public Student() {
+    }
+
+    public Student(String name, boolean retakingExam, int score) {
+        this.name = name;
+        this.retakingExam = retakingExam;
+        this.score = score;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public boolean isRetakingExam() {
+        return retakingExam;
+    }
+
+    public void setRetakingExam(boolean retakingExam) {
+        this.retakingExam = retakingExam;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+    public boolean isFail() {
+        if (score < 60) {
+            return true;
+        }
+        return false;
+    }
+    }
+
