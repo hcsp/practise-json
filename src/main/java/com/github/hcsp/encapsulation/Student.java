7098256261141a50f2b1bbdec5d9a8bb5aad0dfb
JerryWang <1976883731@qq.com>
2021-11-02 17:32:00 +0800

    JSON test

--- src/main/java/com/github/hcsp/encapsulation/Student.java
+++ src/main/java/com/github/hcsp/encapsulation/Student.java
@@ -2,17 +2,20 @@ package com.github.hcsp.encapsulation;
 
 public class Student {
     // 请按照Main类的要求，补全本类
-    /** 姓名 */
+    /**
+     * 姓名
+     */
     private String name;
 
-    /** 是否重考。true为重考，falase为非重考。 */
+    /**
+     * 是否重考。true为重考，falase为非重考。
+     */
     private boolean retakingExam;
 
-    /** 分数 */
+    /**
+     * 分数
+     */
     private int score;
-
-    /** 是否挂科 */
-    private boolean fail;
 
     public String getName() {
         return name;
@@ -35,15 +38,10 @@ public class Student {
     }
 
     public void setScore(int score) {
-        setFail(score < 60);
         this.score = score;
     }
 
     public boolean isFail() {
-        return fail;
-    }
-
-    public void setFail(boolean fail) {
-        this.fail = fail;
+        return score < 60;
     }
 }
