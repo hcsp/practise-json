9d6b66f0da7a2f8016ece8390bbe25d230c15e37
jcbound <784538725@qq.com>
2021-12-11 21:22:58 +0800

    引入依赖包，完成序列化

--- src/main/java/com/github/hcsp/encapsulation/Student.java
+++ src/main/java/com/github/hcsp/encapsulation/Student.java
@@ -5,9 +5,39 @@ public class Student {
     /** 姓名 */
     private String name;
 
-    /** 是否重考。true为重考，falase为非重考。 */
+    /** 是否重考。true为重考，false为非重考。 */
     private boolean retakingExam;
 
     /** 分数 */
     private int score;
+    private boolean fail;
+
+    public String getName() {
+        return name;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public boolean isFail() {
+        return fail;
+    }
+
+    public boolean isRetakingExam() {
+        return retakingExam;
+    }
+
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setRetakingExam(boolean retakingExam) {
+        this.retakingExam = retakingExam;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
 }
