5815a68e3b932d0cd150d0de720dc59846f00e5e
haojing <hitjinghao@gmail.com>
2022-01-25 11:09:32 +0800

    json

--- src/main/java/com/github/hcsp/encapsulation/Student.java
+++ src/main/java/com/github/hcsp/encapsulation/Student.java
@@ -10,4 +10,32 @@ public class Student {
 
     /** 分数 */
     private int score;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setRetakingExam(boolean retakingExam) {
+        this.retakingExam = retakingExam;
+    }
+
+    public boolean isRetakingExam() {
+        return retakingExam;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public boolean isFail() {
+        return score < 60;
+    }
 }
