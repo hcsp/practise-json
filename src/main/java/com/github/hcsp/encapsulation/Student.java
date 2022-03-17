c3ac744a3853a73e4495726a31eeabbc62b9be67
yezhiguang <847786582@qq.com>
2022-03-18 00:00:35 +0800

    序列化和反序列化

--- src/main/java/com/github/hcsp/encapsulation/Student.java
+++ src/main/java/com/github/hcsp/encapsulation/Student.java
@@ -2,6 +2,37 @@ package com.github.hcsp.encapsulation;
 
 public class Student {
     // 请按照Main类的要求，补全本类
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setRetakingExam(boolean retakingExam) {
+        this.retakingExam = retakingExam;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+        if(score>=60){
+            setFail(false);
+        } else{
+            setFail(true);
+        }
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public boolean isRetakingExam() {
+        return retakingExam;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
     /** 姓名 */
     private String name;
 
@@ -10,4 +41,14 @@ public class Student {
 
     /** 分数 */
     private int score;
+
+    public boolean isFail() {
+        return fail;
+    }
+
+    public void setFail(boolean fail) {
+        this.fail = fail;
+    }
+
+    private boolean fail;
 }
