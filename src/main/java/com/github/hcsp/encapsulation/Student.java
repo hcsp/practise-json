16f8d4cad88d3d3a3612d4931b62632953e4c744
lx <iamxliu@163.com>
2022-04-03 08:29:51 +0800

    1

--- src/main/java/com/github/hcsp/encapsulation/Student.java
+++ src/main/java/com/github/hcsp/encapsulation/Student.java
@@ -5,9 +5,54 @@ public class Student {
     /** 姓名 */
     private String name;
 
+    private boolean fail;
+
+
     /** 是否重考。true为重考，falase为非重考。 */
     private boolean retakingExam;
 
     /** 分数 */
     private int score;
+
+    public Student(String name, boolean fail, boolean retakingExam, int score) {
+        this.name = name;
+        this.fail = fail;
+        this.retakingExam = retakingExam;
+        this.score = score;
+    }
+    public Student() {
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public boolean isFail() {
+        return fail;
+    }
+
+    public void setFail(boolean fail) {
+        this.fail = fail;
+    }
+
+    public boolean isRetakingExam() {
+        return retakingExam;
+    }
+
+    public void setRetakingExam(boolean retakingExam) {
+        this.retakingExam = retakingExam;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
 }
