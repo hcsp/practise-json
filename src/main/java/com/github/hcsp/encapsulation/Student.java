fec5e997797524658894fd4454ba086ee45d0ace
jeffrey <908202572@qq.com>
2022-06-06 21:08:28 +0800

    JSON序列化/反序列化实战

--- src/main/java/com/github/hcsp/encapsulation/Student.java
+++ src/main/java/com/github/hcsp/encapsulation/Student.java
@@ -5,9 +5,44 @@ public class Student {
     /** 姓名 */
     private String name;
 
-    /** 是否重考。true为重考，falase为非重考。 */
+    /** 是否重考。true为重考，false为非重考。 */
     private boolean retakingExam;
 
     /** 分数 */
     private int score;
+
+    private boolean fail;
+
+    public boolean isFail() {
+        return fail;
+    }
+
+    public void setFail(boolean fail) {
+        this.fail = fail;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public boolean isRetakingExam() {
+        return retakingExam;
+    }
+
+    public void setRetakingExam(boolean retakingExam) {
+        this.retakingExam = retakingExam;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        setFail(score < 60);
+        this.score = score;
+    }
 }
