7df7ed027ca35a062f5b68dedf8e8c61a348c049
wuchuan <609833577@qq.com>
2022-09-30 10:06:46 +0800

    作业

--- src/main/java/com/github/hcsp/encapsulation/Main.java
+++ src/main/java/com/github/hcsp/encapsulation/Main.java
@@ -1,32 +1,44 @@
 package com.github.hcsp.encapsulation;
 
+import com.google.gson.Gson;
+
+import java.lang.reflect.Type;
+
 public class Main {
-    /*
-         假设你正在为学校开发一个学生分数记录系统
-         你和前端约定的JSON接口格式是：
-         {
-           "name": "张三",
-           "retakingExam": true,
-           "score": 59,
-           "fail": true // 是否挂科，如果分数低于60则返回true，代表挂科
-         }
-         请：
-         1. 设计并完成Student类
-         2. 挑选一种你喜欢的JSON类库，完成序列化/反序列化的方法
-    */
-    public static void main(String[] args) {
-        Student student = new Student();
-        student.setName("张三");
-        student.setScore(60);
-        student.setRetakingExam(true);
-        String json = serialize(student);
+  /*
+       假设你正在为学校开发一个学生分数记录系统
+       你和前端约定的JSON接口格式是：
+       {
+         "name": "张三",
+         "retakingExam": true,
+         "score": 59,
+         "fail": true // 是否挂科，如果分数低于60则返回true，代表挂科
+       }
+       请：
+       1. 设计并完成Student类
+       2. 挑选一种你喜欢的JSON类库，完成序列化/反序列化的方法
+  */
+  public static void main(String[] args) {
+    Student student = new Student();
+    student.setName("张三");
+    student.setScore(60);
+    student.setRetakingExam(true);
+    String json = serialize(student);
 
-        System.out.println(json);
+    System.out.println(json);
 
-        student = deserialize(json);
-    }
-    // 序列化：将Student类转换成JSON字符串
-    public static String serialize(Student student) {}
-    // 反序列化：将JSON字符串转换成Student对象
-    public static Student deserialize(String json) {}
+    student = deserialize(json);
+  }
+
+  // 序列化：将Student类转换成JSON字符串
+  public static String serialize(Student student) {
+    Gson gson = new Gson();
+    return gson.toJson(student);
+  }
+
+  // 反序列化：将JSON字符串转换成Student对象
+  public static Student deserialize(String json) {
+    Gson gson = new Gson();
+    return gson.fromJson(json, Student.class);
+  }
 }
