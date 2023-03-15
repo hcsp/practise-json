da46cb10f5bae178f278ce48ba1ce0186a48b30d
Yang.he <yang.he@homepartners.tech>
2023-03-15 20:19:15 +0800

    - [x] done end

--- src/test/java/com/github/hcsp/encapsulation/Main.java
+++ src/test/java/com/github/hcsp/encapsulation/Main.java
@@ -1,7 +1,5 @@
 package com.github.hcsp.encapsulation;
 
-import com.google.gson.Gson;
-
 public class Main {
     /*
          假设你正在为学校开发一个学生分数记录系统
@@ -29,13 +27,10 @@ public class Main {
     }
     // 序列化：将Student类转换成JSON字符串
     public static String serialize(Student student) {
-        Gson gson = new Gson();
-        return gson.toJson(student);
+        return com.alibaba.fastjson.JSON.toJSONString(student);
     }
-
     // 反序列化：将JSON字符串转换成Student对象
     public static Student deserialize(String json) {
-        Gson gson = new Gson();
-        return gson.fromJson(json, Student.class);
+        return com.alibaba.fastjson.JSON.parseObject(json, Student.class);
     }
 }
