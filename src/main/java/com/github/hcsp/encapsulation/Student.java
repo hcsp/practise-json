2c064a9cb7b7d138c323583196f2f2516471ee66
lyraaaaaaa <Lyraaaaaaaaaaa@gmail.com>
2023-11-03 15:03:58 +0800

    serilization&deserilization

--- src/main/java/com/github/hcsp/encapsulation/Student.java
+++ src/main/java/com/github/hcsp/encapsulation/Student.java
@@ -1,13 +1,33 @@
 package com.github.hcsp.encapsulation;
 
 public class Student {
-    // 请按照Main类的要求，补全本类
-    /** 姓名 */
     private String name;
-
-    /** 是否重考。true为重考，falase为非重考。 */
+    private int score;
     private boolean retakingExam;
 
-    /** 分数 */
-    private int score;
-}
+    public void setRetakingExam(boolean retakingExam) {
+        this.retakingExam = retakingExam;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public boolean isRetakingExam() {
+        return retakingExam;
+    }
+        public void setScore(int score){
+            this.score = score;
+        }
+
+    public int getScore() {
+        return score;
+    }
+
+    public Student() {
+        }
+    }
+
